b724dc9cf8e919653cb21d3b8bae7073a9b71010
KeroppiKun <fuga0809ace@gmail.com>
2024-07-02 17:18:53 +0900

    こっみと

--- Scripts/BattleGround.gd
+++ Scripts/BattleGround.gd
@@ -3,12 +3,23 @@ extends Node2D
 
 @export var BattleGroundTileSize = Vector2i(10,10)
 const Tile_scn = preload("res://Scenes/tile.tscn")
+const Player_scn = preload("res://Scenes/player.tscn")
 var tile_array:Array[Tile]
 signal completeCreateStage
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	CreateStage()
+	#for i in tile_array.size():
+		#if tile_array[i].grid_position == Vector2i(2,2):
+			#tile_array[i]._set_tileType(Tile.TILETYPE.RED)
+	var player : Player = Player_scn.instantiate()
+	for i in tile_array.size():
+		if tile_array[i].grid_position == Vector2i(2,2):
+			player.position = _convert_grid_to_pixel(tile_array[i].grid_position)
+	add_child(player)
+	player._set_tile_array(tile_array)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,3 +40,8 @@ func CreateStage():
 				tile._set_tileType(tile.TILETYPE.RED)
 			tile_array.push_back(tile)
 	emit_signal("completeCreateStage")
+
+func _convert_grid_to_pixel(_grid_position: Vector2i):
+	var _pixel_position = Vector2(_grid_position.x * Grobal.HORIZONAL_TILE_SIZE,_grid_position.y * Grobal.VERTICAL_TILE_SIZE)
+	_pixel_position = _pixel_position + Vector2(Grobal.HORIZONAL_TILE_SIZE / 2,-Grobal.VERTICAL_TILE_SIZE / 2)
+	return _pixel_position
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,62 @@
 extends CharacterBody2D
+class_name Player
 
 var moving = false
 var input_dir
 var grid_position = Vector2i()
 var moveable = true
+var tile_array:Array[Tile]
 
 @onready var nextTileRay: RayCast2D = $NextTileCheckRayCast
 
 func _physics_process(delta: float) -> void:
+	#移動処理
+	_move_update()
+	
+	var groundObj = ($GroundCheckRayCast as RayCast2D).get_collider()
+	if groundObj is Node:
+		if groundObj.is_in_group("Tile"):
+			grid_position = groundObj.grid_position
+	
+	if Input.is_action_just_pressed("ui_select"):
+		_change_tile()
+
+func horizonal_move():
+	if input_dir:
+		#移動方向に合わせてレイを飛ばす向きを設定
+		var desired_step: Vector2 = input_dir * Grobal.HORIZONAL_TILE_SIZE / 2
+		nextTileRay.target_position = desired_step
+		#レイの強制更新
+		nextTileRay.force_raycast_update()
+		#移動先のコリジョン情報を取得
+		var nextTile = nextTileRay.get_collider()
+		#タイルだった場合
+		if nextTile is Tile:
+			#移動可能タイルだった場合移動処理
+			print(nextTile.walkable)
+			if moving == false && nextTile.walkable:
+				moving = true
+				var tween = create_tween()
+				tween.tween_property(self,"position",position + input_dir * Grobal.HORIZONAL_TILE_SIZE,0.1)
+				tween.tween_callback(moving_false)
+
+func vetrtical_move():
+	if input_dir:
+		var desired_step: Vector2 = input_dir * Grobal.VERTICAL_TILE_SIZE / 2
+		nextTileRay.target_position = desired_step
+		nextTileRay.force_raycast_update()
+		var nextTile = nextTileRay.get_collider()
+		if nextTile is Tile:
+			if moving == false && nextTile.walkable:
+				moving = true
+				var tween = create_tween()
+				tween.tween_property(self,"position",position + input_dir * Grobal.VERTICAL_TILE_SIZE,0.1)
+				tween.tween_callback(moving_false)
+
+func moving_false():
+	moving = false
+
+func _move_update():
 	input_dir = Vector2.ZERO
 	
 	if Input.is_action_just_pressed("ui_down"):
@@ -22,46 +71,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_right"):
 		input_dir = Vector2.RIGHT
 		horizonal_move()
-	
-	var groundObj = ($GroundCheckRayCast as RayCast2D).get_collider()
-	if groundObj is Node:
-		if groundObj.is_in_group("Tile"):
-			grid_position = groundObj.grid_position
-	
 
-func horizonal_move():
-	if input_dir:
-		var desired_step: Vector2 = input_dir * Grobal.HORIZONAL_TILE_SIZE / 2
-		nextTileRay.target_position = desired_step
-		nextTileRay.force_raycast_update()
-		var nextTile = nextTileRay.get_collider()
-		if nextTile is Tile:
-			print(nextTile.walkable)
-			if moving == false && nextTile.walkable:
-				moving = true
-				var tween = create_tween()
-				tween.tween_property(self,"position",position + input_dir * Grobal.HORIZONAL_TILE_SIZE,0.1)
-				tween.tween_callback(moving_false)
+func _set_tile_array(_tile_array : Array[Tile]):
+	tile_array = _tile_array
 
-func vetrtical_move():
-	if input_dir:
-		var desired_step: Vector2 = input_dir * Grobal.HORIZONAL_TILE_SIZE / 2
-		nextTileRay.target_position = desired_step
-		nextTileRay.force_raycast_update()
-		if moving == false && nextTileRay.is_colliding():
-			moving = true
-			var tween = create_tween()
-			tween.tween_property(self,"position",position + input_dir * Grobal.VERTICAL_TILE_SIZE,0.1)
-			tween.tween_callback(moving_false)
-
-func moving_false():
-	moving = false
-
-func _check_next_tile():
-	#($NextTileCheckRayCast as RayCast2D).target_position = input_dir * 32
-	if ($NextTileCheckRayCast as RayCast2D).is_colliding():
-		var nextObj = ($NextTileCheckRayCast as RayCast2D).get_collider()
-		if nextObj is Node:
-			if nextObj.is_in_group("Tile"):
-				return nextObj
-	return null
+func _change_tile():
+	for i in tile_array.size():
+		if tile_array[i].grid_position == grid_position + Vector2i(1,0):
+			tile_array[i]._set_tileType(Tile.TILETYPE.RED)
